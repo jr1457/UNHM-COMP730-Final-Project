707c9b88b27ef524da3c4b41638e7e7b0601f7c0
James Rooney <jr1457@wildcats.unh.edu>
2023-04-19 10:02:42 -0400

    In class, created createNoteTest junit test case as well as cleaned up the repo from various testing files

---
+++ NotesApp2/src/CreateNoteTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CreateNoteTest {
+
+	@Test
+	public void test() {
+		String testNoteName = "test";
+		String testNoteData = "hello world";
+		Client client = new Client(testNoteName, testNoteData);
+		client.buildImmediately();
+		CreateNote.create(client);
+		
+		Read r = new Read("test.txt");
+		String contents = r.readFromFile();
+		assertEquals(contents, testNoteData);
+		//fail("Not yet implemented");
+	}
+
+}
